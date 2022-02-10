e53857f8162778a4e9c66165b966e0546708dba1
BhoomikaThippani <bhoomika.thippani@gmail.com>
2022-02-10 22:51:58 +0530

    my day10 java prgs

---
+++ Day10/Day10/LambdaExpressionDemo.java
@@ -0,0 +1,24 @@
+package com.demo.Day10;
+
+@FunctionalInterface
+interface Drawables{
+	
+	public void  draw();
+	
+}
+
+
+public class LambdaExpressionDemo {
+		
+	public static void main(String ar[]) {
+		
+		int width = 25;
+		//functional style
+		Drawables d =() ->{
+			
+			System.out.println("draw "+width);
+		};
+		d.draw();
+	}
+
+}
